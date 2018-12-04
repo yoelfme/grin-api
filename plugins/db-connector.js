6524990e0dc7d043b9fef6e701be27be58cf33a9
Yoel Monzon <yoelfme@hotmail.com>
2018-12-03 19:42:52 -0600

    [add] plugin used to connect to MongoDB

---
+++ plugins/db-connector.js
@@ -0,0 +1,42 @@
+const bucker = require('bucker')
+const joi = require('joi')
+const mongoose = require('mongoose')
+
+const validateSchema = joi.object().keys({
+  host: joi.string().required(),
+  port: joi.string().required(),
+  database: joi.string().required(),
+  debug: joi.boolean(),
+})
+
+module.exports = {
+  register: (server, options) => {
+    const validate = joi.validate(options, validateSchema)
+
+    const logger = bucker.createLogger({
+      name: '/plugins/db-connector',
+      console: options.debug || false,
+    })
+
+    if (!validate.error) {
+      const uri = `mongodb://${options.host}:${options.port}/${
+        options.database
+      }`
+      logger.info(`Trying to connect to: ${uri}`)
+
+      try {
+        mongoose.connect(
+          uri,
+          { useNewUrlParser: true },
+        )
+        logger.info(`Successful connection to: ${uri}`)
+      } catch (error) {
+        logger.error(`Error trying to connect to: ${uri} with error ${error}`)
+        throw error
+      }
+    } else {
+      throw validate.error
+    }
+  },
+  name: 'db',
+}
